5a9daf1d63c09f0f508ea90aef0aec334a7158de
Nirjal22 <bistanirjal978@gmail.com>
2025-08-12 20:24:26 +0545

    projects cards

--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -2,26 +2,24 @@ import React, { memo } from 'react'
 
 function Cont({ name, context }: { name: string, context: string }) {
   return (
-    <li>
-      <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700">
-        <a href="#">
-          <img className="rounded-t-lg" src="/docs/images/blog/image-1.jpg" alt={name} />
-        </a>
-        <div className="p-5">
-          <a href="#">
-            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{name}</h5>
-          </a>
-          <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">{context}</p>
-          <a href="#" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-            Read more
-            <svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-              <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
-            </svg>
-          </a>
+      <li className='m-2'>
+        <div className="max-w-sm bg-gray-800 border border-gray-700 rounded-lg shadow-sm dark:bg-white dark:border-gray-200">
+          <div className="p-5">
+            <a href="#">
+              <h5 className="mb-2 text-2xl font-bold tracking-tight text-white dark:text-gray-900">{name}</h5>
+            </a>
+            <p className="mb-3 font-normal text-gray-400 dark:text-gray-700">{context}</p>
+            <a href="#" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center
+           text-white bg-blue-600 rounded-lg hover:bg-blue-700 focus:ring-4 focus:outline-none
+            focus:ring-blue-800 dark:bg-blue-700 dark:hover:bg-blue-800 dark:focus:ring-blue-300">
+              Read more
+              <svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
+              </svg>
+            </a>
+          </div>
         </div>
-      </div>
-
-    </li>
+      </li>
   )
 }
 
@@ -32,13 +30,13 @@ const page = () => {
     { name: 'APIs_JWTAuth', context: 'Django backend. JWT Authentication and CRUD operation. API checked using postman.' },
     { name: 'Weather_Webapp', context: 'Learned to use API and get the current weather condition of particular place.' },
     { name: 'Turtle Graphics Program', context: 'Java Swing is used. Understanding of OOP and GUI' },
-    { name: 'Bank Management System', context: 'C++ project. Banking system.' },
+    { name: 'Bank Management System', context: 'C++ project. Banking system. Understanding core of C++.' },
   ]
   return (
     <div>
       <h1 className='text-4xl m-2'>Portfolio</h1>
       {/* cards */}
-      <ul>
+      <ul className='grid grid-cols-3'>
         {content.map((conte, index) => (
           <Cont key={index} context={conte.context} name={conte.name} />
         ))}
